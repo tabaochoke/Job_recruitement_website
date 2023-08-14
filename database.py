ccf839c501c76490f70f5ad3dd1fe4e87fc6a78f
tabaochoke <108040902+tabaochoke@users.noreply.github.com>
2023-08-14 13:45:56 +0000

    serect keys

--- database.py
+++ database.py
@@ -1,11 +1,16 @@
 import mysql.connector
 from mysql.connector import Error
+import os
 
+host = os.environ['host']
+database = os.environ['database']
+user = os.environ['user']
+print(host)
 try:
   connection = mysql.connector.connect(
-    host='aws.connect.psdb.cloud',
-    database='website_test_db',
-    user='3a90179ffah7xxq81do9',
+    host=host,
+    database=database,
+    user=user,
     password='pscale_pw_uWGgz0ePGJAs8gG41PyxMd4effsi5Ndogu3K7NkFV2r')
   if connection.is_connected():
 
