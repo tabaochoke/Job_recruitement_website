aa592d5227663741faf275366535acf1aa296b7a
tabaochoke <108040902+tabaochoke@users.noreply.github.com>
2023-08-17 13:00:49 +0000

    Success connect database

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify
-from database import cursor
+from flask import Flask, render_template, jsonify, request
+from database import  JOBS , insert_job
 
 app = Flask(__name__)
 
@@ -28,22 +28,11 @@ app = Flask(__name__)
 #     'salary' : '$120000'
 #   }
 # ]
-JOBS = []
-for row in cursor:
-  job = {}
-  job['id'] = int(row[0]) - 1
-  job['title'] = row[1]
-  job['location'] = row[2]
-  job['salary'] = row[3]
-  job['repon'] = row[5]
-  job['require'] = row[6]
-  job['create'] = row[7]
-  job['update'] = row[8]
-  JOBS.append(job)
 
 
-@app.route('/')
+@app.route('/' , methods=['GET', 'POST'])
 def hello_world():
+  
   return render_template('home.html', JOBS=JOBS, company="Jovian")
 
 
@@ -52,15 +41,32 @@ def return_jobs():
   return jsonify(JOBS)
 
 
-@app.route('/jobs/<id>')
-def return_job(id):
+@app.route('/jobs/<id>' )
+def return_job(id ):
   try:
     job_id = int(id)
     job = JOBS[job_id]
+    
     return render_template('job_detail.html', job=job)
   except ValueError:
     return 'Invalid job ID'
-  
 
+@app.route('/process', methods=['POST'])
+def process():
+  
+  first_name = request.form.get('First Name:')
+  # process the data using Python code
+  last_name = request.form.get('Last Name:')
+  user_name = request.form.get('Username:')
+  email = request.form.get('Email:')
+  address = request.form.get('Address:')
+  cv_url = request.form.get('CV URL:')
+  country = request.form.get('Country:')
+  state = request.form.get('State:')
+  zip = request.form.get('Zip:')
+  print  (first_name , last_name , user_name , email , address , cv_url , country , state , zip)
+  insert_job (first_name, last_name, user_name, email, address, cv_url, country, state, zip)
+  return render_template('home.html', JOBS=JOBS, company="Jovian")
+  
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -7,8 +7,6 @@ database = os.environ['database']
 user = os.environ['user']
 pass_w = os.environ['pass']
 
-
-print(host)
 try:
   connection = mysql.connector.connect(
     host=host,
@@ -16,13 +14,56 @@ try:
     user=user,
     password=pass_w)
   if connection.is_connected():
-
     cursor = connection.cursor()
-
     query = ("SELECT * FROM jobs;")
     cursor.execute(query)
+    JOBS = []
+    for row in cursor:
+      job = {}
+      job['id'] = int(row[0]) - 1
+      job['title'] = row[1]
+      job['location'] = row[2]
+      job['salary'] = row[3]
+      job['repon'] = row[5]
+      job['require'] = row[6]
+      job['create'] = row[7]
+      job['update'] = row[8]
+      JOBS.append(job)
+
 except Error as e:
   print("Error while connecting to MySQL", e)
+finally:
+  if connection.is_connected():
+    cursor.close()
+    connection.close()
+    print("MySQL connection is closed")
+
+
+
+def insert_job (first_name, last_name, user_name, email, address, cv_url, country, state, zip) :
+  try:
+    connection = mysql.connector.connect(
+    host=host,
+    database=database,
+    user=user,
+    password=pass_w)
+    if connection.is_connected():
+      cursor = connection.cursor()
+      query = ( """INSERT INTO application2
+      (first_name, last_name, user_name, email, address, cv_url, country, state, zip)
+ VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);"""  )
+      
+      value = (first_name, last_name, user_name, email, address, cv_url, country, state, zip)
+      cursor.execute(query , value)
+      emp_no = cursor.lastrowid
+      connection.commit ()
+      print (query)
+  except Error as e:
+    print("Error while connecting to MySQL", e)
+
+  cursor.close()
+  connection.close()
+  print("MySQL connection is closed")
 # finally:
 #   if connection.is_connected():
 #     cursor.close()
